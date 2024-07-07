973d4542ecd67711dc108874bce6bc31c133066f
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-07 20:11:11 +0200

    [UPD] add style and contents at header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,4 +1,4 @@
-<header>
+<header class="py-2">
     <div class="container-full py-3 px-5">
         <div class="row">
             <div class="content d-flex justify-content-between align-items-center text-center">
@@ -10,7 +10,7 @@
                         <li><a href="{{ route('about') }}">About</a></li>
                     </ul>
                 </div>
-                <div class="col"> <img class="logo" src="{{ Vite::asset('resources/img/logo.jpg') }}" alt="pic">
+                <div class="col"> <img class="logo" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="pic">
                 </div>
                 <div class="col-4">
                     <ul class=" d-flex justify-content-end gap-3 align-items-center list-unstyled text-white">
