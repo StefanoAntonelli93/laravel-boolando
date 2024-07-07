0d21c140449d95dfb5d6b4474779b00b7fe9f458
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-07 19:02:55 +0200

    [UPD] add content and style at footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,25 @@
-<footer class="text-center p-5 bg-dark text-white">
-    <div class="fw-semibold">Powered by Stefano Antonelli</div>
+<footer class="bg-dark text-white py-3">
+    <div class="container-full py-3 px-5">
+        <div class="row d-flex justify-content-between text-align-center">
+            <div class="col-8">
+                <div>Boolando srl</div>
+                <ul class="d-flex gap-4 list-unstyled justify-content-start py-1">
+                    <li>Informazioni legali</li>
+                    <li>Informativa sulla privacy</li>
+                    <li>Diritto di recesso</li>
+                </ul>
+            </div>
+            <div class="col text-end">
+                <div>Trovaci anche su</div>
+                <ul class="d-flex gap-2 list-unstyled justify-content-end py-1">
+                    <li><i class="fa-brands fa-square-facebook"></i></li>
+                    <li><i class="fa-brands fa-square-instagram"></i></li>
+                    <li><i class="fa-brands fa-square-twitter"></i></li>
+                    <li><i class="fa-brands fa-square-pinterest"></i></li>
+                    <li><i class="fa-brands fa-youtube"></i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </footer>
