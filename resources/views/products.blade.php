057523d8dc36a7755355d7e98504b20bba3ddc0c
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-08 02:40:28 +0200

    [UPD] create cards with details for products.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -7,8 +7,19 @@ PRODOTTI-BOOLANDO
 
 @section('products')
 <div class="container py-3">
-    <h2>{{ $title }}</h2>
+    <h2 class="pt-3 pb-4">{{ $title }}</h2>
+    <ul class="d-flex flex-wrap list-unstyled gap-3 justify-content-center">
+        @foreach($dress as $product)
+        <li>
+            <!-- carico le immagini dell'array implementando assets vite + chiave array -->
+            <img class="cards-image" src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" alt="{{ $product['name'] }}">
+            <div>{{ $product['brand'] }}</div>
+            <div class="fw-semibold text-uppercase">{{ $product['name'] }}</div>
+            <div class="discount-price fw-semibold pb-5">{{ $product['price'] }}</div>
+        </li>
+        @endforeach
+
+    </ul>
 
 </div>
-sono il contenuto di products
 @endsection
